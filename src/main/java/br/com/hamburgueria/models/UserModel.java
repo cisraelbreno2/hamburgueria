8f5d304dd7e88a0abc341e2d10b4f9447309847f
junioorosa <86578051+junioorosa@users.noreply.github.com>
2022-09-01 22:29:37 -0300

    Criado UserModel

--- src/main/java/br/com/hamburgueria/models/UserModel.java
+++ src/main/java/br/com/hamburgueria/models/UserModel.java
@@ -1,4 +1,29 @@
 package br.com.hamburgueria.models;
 
-public class UserModel {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class UserModel implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String name;
+	private String cpf;
+	private String password;
+	private String login;
+	
 }
